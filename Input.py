af5c20803491e09cffcbc9fdc3cae4bb24bf2d3d
DrSpacemanMD <Johnt717@gmail.com>
2022-01-31 23:31:35 +0000

    started on SNR

--- Input.py
+++ Input.py
@@ -3,9 +3,10 @@
 import Analysis
 import Compute_Distortion
 import sys
+import SNRCalc
 
 #Set up the distortion calc by passing the folder with all the DICOMS and the sequence of interest.
-ComputeDistortion = Compute_Distortion.DistortionCalculation("Flipped" , "3D Sag T1 BRAVO_90-y-rot") 
+ComputeDistortion = Compute_Distortion.DistortionCalculation("TestData" , "3D Sag T1 BRAVO Geom Core") 
 
 #The isaldnChecker method is a bit more sensitive to background noise so its usually good to shortern the search width
 #ComputeDistortion.searchWidth = 3.9
@@ -17,24 +18,21 @@ ComputeDistortion = Compute_Distortion.DistortionCalculation("Flipped" , "3D Sag
 ComputeDistortion.BinariseMethod = "Constant"
 #Set up the analysis script, this takes the computed distacnes and output metrics. Pass a tag for the calculaton (used for naming saved images etc) and the previously constructed distirtion calc class
 Analyse = Analysis.AnalysisResults("FlippedData",ComputeDistortion)
-ComputeDistortion.Threshold=2000
+ComputeDistortion.Threshold=3000
 #Call this function to get all the centre of the distances
 ComputeDistortion.GetFudicalSpheres()
 
 #Call this function to check the points which have been found, shows a plotting dialog you can navigate with right/left keyboard arrows
-Analyse.CheckPoints()
+#Analyse.CheckPoints()
 
 #Call this function to calculate the distances
-ComputeDistortion.GetDistances()
+#ComputeDistortion.GetDistances()
 
 ##Call this functuion to analyse the distances and compute the metrics
-Analyse.DistortionAnalysis()
+#Analyse.DistortionAnalysis()
 
 #Call this function to print the computed metrics to screen
-Analyse.PrintToScreen()
+#Analyse.PrintToScreen()
 
-#Call this function to save the metric data to a csv that is updated for each run
-#Analyse.OutputPeriodicData("DistortionData.csv")
-
-#Plot the csv so we cna see data over time
-#Analyse.PlotCSV("DistortionData.csv")
+SNR = SNRCalc.SNR(ComputeDistortion)
+SNR.ComputerSNR()
---
+++ SNRCalc.py
@@ -0,0 +1,71 @@
+import pydicom
+import matplotlib.pyplot as plt
+import glob
+import numpy as np
+from skimage.feature import blob_dog, blob_log, blob_doh
+import math
+from skimage.filters import threshold_minimum
+import skimage.filters 
+import sys
+from sklearn.cluster import KMeans
+from dataclasses import dataclass
+import matplotlib
+import copy
+import datetime
+from scipy import ndimage
+import matplotlib.patches as patches
+
+
+class SNR():
+		
+	#initalises the class with a bunch of variables.
+	def __init__(self, DistortionObj):
+		self.DistortionObj = DistortionObj
+
+	def ComputerSNR(self):
+		Spheres = self.DistortionObj.SphereLocations[0] #get spheres in the top slice 
+		averagex = []
+		averagey = []
+		averagez = []
+
+		boxsize = [50,50]
+
+		for xyz in Spheres:
+			averagex.append(xyz[0])
+			averagey.append(xyz[1])
+			averagez.append(xyz[2])
+
+		averagex= int(round( (float)(sum(averagex)/len(averagex))))
+		averagey= int(round( (float)(sum(averagey)/len(averagey))))
+		averagez= int(round( (float)(sum(averagez)/len(averagez))))
+		
+		Image = self.DistortionObj.GetCorSlice( int(round(averagez)))
+
+		Signal = Image[int(averagey-boxsize[1]/2):int(averagey+boxsize[1]/2),int(averagex-boxsize[0]/2):int(averagex+boxsize[0]/2)]
+		
+		Noise1 = Image[int(0):int(boxsize[1]),int(0):int(boxsize[0])]
+		Noise2 = Image[int(Image.shape[0]-1-boxsize[0]):int(Image.shape[0]-1),int(0):int(boxsize[0])]
+		Noise3 = Image[int(0):int(boxsize[1]),int(Image.shape[1]-1-boxsize[1]):int(Image.shape[1]-1)]
+		Noise4 = Image[int(Image.shape[0]-1-boxsize[0]):int(Image.shape[0]-1),int(Image.shape[1]-1-boxsize[1]):int(Image.shape[1]-1)]
+
+		#Image[int(0):int(boxsize[1]),int(0):int(boxsize[0])] = 7000000
+		#Image[int(Image.shape[0]-1-boxsize[0]):int(Image.shape[0]-1),int(0):int(boxsize[0])] = 7000000
+		#Image[int(0):int(boxsize[1]),int(Image.shape[1]-1-boxsize[1]):int(Image.shape[1]-1)] = 7000000
+		#Image[int(Image.shape[0]-1-boxsize[0]):int(Image.shape[0]-1),int(Image.shape[1]-1-boxsize[1]):int(Image.shape[1]-1)] = 7000000
+
+
+		SigRect = patches.Rectangle((averagex-boxsize[0]/2, averagey-boxsize[1]/2), boxsize[0], boxsize[1], linewidth=1, edgecolor='r', facecolor='none')
+		Noise1Rect = patches.Rectangle((0, 0), boxsize[0], boxsize[1], linewidth=1, edgecolor='r', facecolor='none')
+		Noise2Rect = patches.Rectangle((Image.shape[1]-1-boxsize[0], 0), boxsize[0], boxsize[1], linewidth=1, edgecolor='r', facecolor='none')
+		#Noise3Rect = patches.Rectangle((0, Image.shape[1]-1-boxsize[1]), boxsize[0], boxsize[1], linewidth=1, edgecolor='r', facecolor='none')
+		#Noise4Rect = patches.Rectangle((Image.shape[0]-1-boxsize[0], Image.shape[1]-1-boxsize[1]), boxsize[0], boxsize[1], linewidth=1, edgecolor='r', facecolor='none')
+						
+		plt.gca().add_patch(SigRect)
+		plt.gca().add_patch(Noise1Rect)
+		plt.gca().add_patch(Noise2Rect)
+		#plt.gca().add_patch(Noise3Rect)
+		#plt.gca().add_patch(Noise4Rect)
+
+		plt.imshow(Image)
+		plt.show()
+		sys.exit()
